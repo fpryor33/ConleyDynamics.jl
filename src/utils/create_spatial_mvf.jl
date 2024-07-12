b39e378d2c3f4cc5f32af3252b3c327386bda715
Thomas Wanner <twanner@gmu.edu>
2024-07-12 20:43:34 +0200

    started edge2region

--- src/utils/create_spatial_mvf.jl
+++ src/utils/create_spatial_mvf.jl
@@ -218,6 +218,205 @@ end
 
 ###############################################################################
 
+function spatial_mvf_edge2region(eindex::Int, lc::LefschetzComplex,
+                                 coords::Vector{Vector{Float64}}, vf)
+    #
+    # Find the 3d regions which can be entered from an edge
+    #
+
+    npts = 5   # Look at npts-1 points along the edge to determine the flow
+
+    # Determine the boundary of the edge
+
+    v1, v2 = lefschetz_boundary(lc, eindex)
+
+    # Determine the adjacent faces and regions
+
+    openhull = lefschetz_openhull(lc, [eindex])
+    openhull_dims = lc.dimensions[openhull]
+    adfaces = openhull[findall(isequal(2),openhull_dims)]
+    adregns = openhull[findall(isequal(3),openhull_dims)]
+
+    # Determine the adjacent edges for each edge vertex
+    # In each case, remove the edge 'eindex'
+
+    openhull1 = lefschetz_openhull(lc, [v1])
+    openhull1_dims = lc.dimensions[openhull1]
+    adedges1 = setdiff(openhull1[findall(isequal(1),openhull1_dims)], [eindex])
+
+    openhull2 = lefschetz_openhull(lc, [v2])
+    openhull2_dims = lc.dimensions[openhull2]
+    adedges2 = setdiff(openhull2[findall(isequal(1),openhull2_dims)], [eindex])
+
+    # Create vectors of adjacent vertices for v1 and v2
+    # In each case, remove v2 and v1, respectively
+
+    adverts1 = setdiff(lefschetz_skeleton(lc, adedges1, 0), [v1])
+    adverts2 = setdiff(lefschetz_skeleton(lc, adedges2, 0), [v2])
+
+    # Loop through the regions to find all target regions
+
+    targetregions = Vector{Int}()
+    cv1 = coords[v1]
+    cv2 = coords[v2]
+
+    for r in adregns
+
+        # Determine the faces f1 and f2 of the region which
+        # contain the given edge eindex
+
+        f1, f2 = intersect(lefschetz_boundary(lc, r), adfaces)
+
+        # For each of these faces, determine its edges which are
+        # adjacent to v1 and v2, but different from eindex
+
+        f1bnd = lefschetz_boundary(lc, f1)
+        e11 = first(intersect(f1bnd, adedges1))
+        e12 = first(intersect(f1bnd, adedges2))
+
+        f2bnd = lefschetz_boundary(lc, f2)
+        e21 = first(intersect(f2bnd, adedges1))
+        e22 = first(intersect(f2bnd, adedges2))
+
+        # For each of these edges, determine its boundary vertex
+        # which is different from v1 or v2
+        # Notation: vXY is vertex on face X opposite to vertex vY
+
+        v11 = first(setdiff(lefschetz_boundary(lc, e11), [v1, v2]))
+        v12 = first(setdiff(lefschetz_boundary(lc, e12), [v1, v2]))
+        v21 = first(setdiff(lefschetz_boundary(lc, e21), [v1, v2]))
+        v22 = first(setdiff(lefschetz_boundary(lc, e22), [v1, v2]))
+
+
+
+
+
+
+
+
+
+
+
+    end
+
+
+
+
+
+
+    # Loop through the regions, compute the barycentric
+    # coordinates, and add the relevant regions
+
+    targetregions = Vector{Int}()
+    c0 = coords[vindex]
+    vfc0 = vf(c0)
+    tc0 = c0 .+ vfc0
+
+    for k in adregns
+
+        # Determine the vertices of the region
+
+        regionvertices = lefschetz_skeleton(lc, [k], 0)
+        adregionvertices = intersect(regionvertices, adverts)
+
+        # Determine the barycentric coordinates
+
+        p1, p2, p3 = adregionvertices
+        c1, c2, c3 = spatial_barycentric_coords(tc0, coords[p1], coords[p2], coords[p3], c0)
+
+        # Add the region if necessary
+
+        if (c1 >= 0.0) & (c2 >= 0.0) & (c3 >= 0.0)
+            push!(targetregions, k)
+        end
+    end
+
+    # Return the target regions
+
+    return targetregions
+
+
+
+
+
+
+    # Create normal vector to the edge
+
+    pvec = coords[v2] .- coords[v1]
+    nvec = [-pvec[2], pvec[1]]
+
+    # Determine sign of the dot products along the edge
+
+    dp_is_p = false
+    dp_is_n = false
+    dp_is_z = false
+
+    nptsf = Float64(npts)
+    p0 = coords[v1]
+    for k = 0:npts
+        pc = p0 .+ ((k / nptsf) .* pvec)
+        pvf = vf(pc)
+        dprod = pvf[1]*nvec[1] + pvf[2]*nvec[2]
+        if dprod > 0.0
+            dp_is_p = true
+        elseif dprod < 0.0
+            dp_is_n = true
+        elseif (k>0) & (k<npts)
+            dp_is_z = true
+        end
+    end
+
+    # Decide whether an adjacent region could be a target
+
+    targetregions = Vector{Int}()
+    midpoint = 0.5 .* coords[v1] .+ 0.5 .* coords[v2]
+
+    for k in adregions
+
+        # Determine the barycenter of the region
+
+        regionvertices = lefschetz_skeleton(lc, [k], 0)
+        nv = 0.0
+        bpoint = [0.0, 0.0]
+        for m in regionvertices
+            bpoint = bpoint .+ coords[m]
+            nv = nv + 1.0
+        end
+        bpoint = bpoint / nv
+
+        # Determine the dot product of the segment from the
+        # midpoint to the barycenter with the normal vector
+
+        dprod = (bpoint[1] - midpoint[1]) * nvec[1] +
+                (bpoint[2] - midpoint[2]) * nvec[2]
+
+        # Add the region, if necessary
+
+        if (dprod > 0.0) & (dp_is_p | dp_is_z)
+            push!(targetregions, k)
+        end
+
+        if (dprod < 0.0) & (dp_is_n | dp_is_z)
+            push!(targetregions, k)
+        end
+    end
+    
+    # Return the target regions
+
+    return targetregions
+end
+
+###############################################################################
+
+
+
+
+
+
+
+
+###############################################################################
+
 function planar_mvf_edge2region(eindex::Int, lc::LefschetzComplex,
                                 coords::Vector{Vector{Float64}}, vf)
     #
