80d727befc7a6c7a99a31af45c4a4d1470b8c1cc
fpryor33 <fpryor@gmu.edu>
2025-09-17 20:12:16 -0400

    Update plot_planar_simplicial_morse.jl
    Modified original function to include an optional Conley index color code.

--- src/plots/plot_planar_simplicial_morse.jl
+++ src/plots/plot_planar_simplicial_morse.jl
@@ -9,7 +9,8 @@ export plot_planar_simplicial_morse
                                  [vfac::Real=1.2,]
                                  [sfac::Real=0,]
                                  [pdim::Vector{Bool}=[false,true,true],]
-                                 [pv::Bool=false])
+                                 [pv::Bool=false]
+                                 [ci::Bool=false])
 
 Create an image of a planar simplicial complex, together with
 Morse sets, or also selected multivectors.
@@ -27,24 +28,28 @@ The optional constants `hfac` and `vfac` contain the horizontal and
 vertical scale vectors for the margins, while `sfac` describes a uniform
 scale. If `sfac=0` the latter is automatically determined. The vector `pdim`
 specifies in which dimensions cells are drawn; the default only shows edges
-and triangles. Finally if one passes the argument `pv=true`, then in addition
-to saving the file a preview is displayed.
+and triangles. If one passes the argument `pv=true`, then in addition
+to saving the file a preview is displayed. Lastly, passing the argument 'ci=true' will 
+color code the Morse sets according to their respective Conley indices.
 """
+
+
 function plot_planar_simplicial_morse(sc::LefschetzComplex,
-                                      coords::Vector{<:Vector{<:Real}},
-                                      fname::String,
-                                      morsesets::CellSubsets;
-                                      hfac::Real=1.2,
-                                      vfac::Real=1.2,
-                                      sfac::Real=0,
-                                      pdim::Vector{Bool}=[false,true,true],
-                                      pv::Bool=false)
+                                       coords::Vector{<:Vector{<:Real}},
+                                       fname::String,
+                                       morsesets::CellSubsets;
+                                       hfac::Real=1.2,
+                                       vfac::Real=1.2,
+                                       sfac::Real=0,
+                                       pdim::Vector{Bool}=[false,true,true],
+                                       pv::Bool=false,
+                                       ci::Bool=false)
     #
     # Create an image of a planar simplicial complex
     #
 
     # Create proper coordinates
-   
+    
     cx0 = minimum([c[1] for c in coords])
     cx1 = maximum([c[1] for c in coords])
     cy0 = minimum([c[2] for c in coords])
@@ -79,7 +84,7 @@ function plot_planar_simplicial_morse(sc::LefschetzComplex,
     end
 
     # Create the image
-   
+    
     Drawing(figw, figh, fname)
     background("white")
     sethue("black")
@@ -90,11 +95,13 @@ function plot_planar_simplicial_morse(sc::LefschetzComplex,
         cdim = sc.dimensions[k]
         if (cdim == 0) && pdim[1]
             setcolor("royalblue4")
+            setopacity(0.4)
             circle(points[k], 5, action = :fill)
         elseif (cdim == 1) && pdim[2]
             k1 = cellvertices[k][1]
             k2 = cellvertices[k][2]
             setcolor("royalblue3")
+            setopacity(0.4)
             line(points[k1],points[k2])
             strokepath()
         elseif (cdim == 2) && pdim[3]
@@ -102,8 +109,9 @@ function plot_planar_simplicial_morse(sc::LefschetzComplex,
             k2 = cellvertices[k][2]
             k3 = cellvertices[k][3]
             setcolor("steelblue1")
+            setopacity(0.4)
             poly([points[k1],points[k2],points[k3]],
-                       action = :fill; close=true)
+                 action = :fill; close=true)
         end
     end
 
@@ -114,39 +122,92 @@ function plot_planar_simplicial_morse(sc::LefschetzComplex,
     else
         msI = convert_cellsubsets(sc, morsesets)
     end
+    
+    # Color code Morse sets if 'ci' is flagged
+    if ci == true
+        col1 = colorant"rgb(51,34,136)"     #color-blind safe 'indigo'
+        col2 = colorant"rgb(221,204,119)"   #color-blind safe 'tan' 
+        col3 = colorant"rgb(17,119,51)"     #color-blind safe 'moss green'
+        col4 = colorant"rgb(204,102,119)"  #color-blind safe 'salmon'
+        col5 = colorant"rgb(135,34,85)"     #color-blind safe 'maroon'
+        col6 = colorant"rgb(153,153,51)"    #color-blind safe 'asparagus'
+        c1 = [1,0,0]  #asym stable
+        c2 = [0,1,0]  #1d unstable
+        c3 = [0,0,1]  ### unstable
+        c4 = [1,1,0]  ##  stable periodic
+        c5 = [0,1,1]  # unstable periodic
+        
+        # Pass the single Morse set `msI[m]`
+        for m in eachindex(msI)
+            current_ci = conley_index(sc, msI[m])
+            
+            if (current_ci == c1)
+                setcolor(col1)
+            elseif (current_ci == c2)     
+                setcolor(col2)
+            elseif (current_ci == c3)     
+                setcolor(col3)
+            elseif (current_ci == c4)     
+                setcolor(col4)
+            elseif (current_ci == c5)     
+                setcolor(col5)        
+            else 
+                setcolor(col6)     
+            end
+            
+            setopacity(0.6)
+            for k in msI[m]
+                cdim = sc.dimensions[k]
+                if (cdim == 0) && pdim[1]
+                    circle(points[k], 5, action = :fill)
+                elseif (cdim == 1) && pdim[2]
+                    k1 = cellvertices[k][1]
+                    k2 = cellvertices[k][2]
+                    line(points[k1],points[k2])
+                    strokepath()
+                elseif (cdim == 2) && pdim[3]
+                    k1 = cellvertices[k][1]
+                    k2 = cellvertices[k][2]
+                    k3 = cellvertices[k][3]
+                    poly([points[k1],points[k2],points[k3]],
+                         action = :fill; close=true)
+                end
+            end
+        end
 
-    col1 = colorant"royalblue4"
-    col2 = colorant"royalblue3"
-    col3 = colorant"steelblue1"
-    cols = distinguishable_colors(length(msI), [col1,col2,col3], dropseed=true)
 
-    for m in eachindex(msI)
-        setcolor(cols[m])
-        setopacity(0.6)
-        for k in msI[m]
-            cdim = sc.dimensions[k]
-            if (cdim == 0) && pdim[1]
-                circle(points[k], 5, action = :fill)
-            elseif (cdim == 1) && pdim[2]
-                k1 = cellvertices[k][1]
-                k2 = cellvertices[k][2]
-                line(points[k1],points[k2])
-                strokepath()
-            elseif (cdim == 2) && pdim[3]
-                k1 = cellvertices[k][1]
-                k2 = cellvertices[k][2]
-                k3 = cellvertices[k][3]
-                poly([points[k1],points[k2],points[k3]],
-                           action = :fill; close=true)
+    else # if ci == false
+        col1 = colorant"royalblue4"
+        col2 = colorant"royalblue3"
+        col3 = colorant"steelblue1"
+        cols = distinguishable_colors(length(msI), [col1,col2,col3], dropseed=true)
+        
+        for m in eachindex(msI)
+            setcolor(cols[m])
+            setopacity(0.6)
+            for k in msI[m]
+                cdim = sc.dimensions[k]
+                if (cdim == 0) && pdim[1]
+                    circle(points[k], 5, action = :fill)
+                elseif (cdim == 1) && pdim[2]
+                    k1 = cellvertices[k][1]
+                    k2 = cellvertices[k][2]
+                    line(points[k1],points[k2])
+                    strokepath()
+                elseif (cdim == 2) && pdim[3]
+                    k1 = cellvertices[k][1]
+                    k2 = cellvertices[k][2]
+                    k3 = cellvertices[k][3]
+                    poly([points[k1],points[k2],points[k3]],
+                         action = :fill; close=true)
+                end
             end
         end
     end
 
     # Finish the drawing, and preview if desired
-
     finish()
     if pv
         preview()
     end
 end
-
